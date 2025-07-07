a196a66eaf5d37baad0b2689ef32128015599d84
Yusuf Osman Avcı <yusufavci0516@gmail.com>
2025-07-07 14:24:15 +0300

    Basic enemy following system added

--- Assets/Scripts/EnemyScripts/EnemyController.cs
+++ Assets/Scripts/EnemyScripts/EnemyController.cs
@@ -12,13 +12,33 @@ public class EnemyController : MonoBehaviour
 
     private void Update()
     {
-        EnemyPatrol();
+        if (!enemyValues.IsPlayerInRange)
+        {
+            EnemyPatrol();
+            PlayerDetector();
+        }
+        else
+        {
+            PlayerDetector();
+            PlayerTrackker();
+        }
     }
 
     private void EnemyPatrol()
     {
+        bool AtoB = enemyValues.APoint.position.x < enemyValues.BPoint.position.x;
         transform.position = Vector2.MoveTowards(transform.position, enemyValues.BPoint.transform.position, enemyValues.enemySpeed * Time.deltaTime);
-        if(Vector2.Distance(transform.position, enemyValues.BPoint.transform.position) < 0.1f)
+        if (AtoB)
+        {
+            enemyValues.IsFacingRight = true;
+            enemyValues.enemySpriteRenderer.flipX = false;
+        }
+        else
+        {
+            enemyValues.IsFacingRight = false;
+            enemyValues.enemySpriteRenderer.flipX = true;
+        }
+        if (Vector2.Distance(transform.position, enemyValues.BPoint.transform.position) < 0.1f)
         {
             (enemyValues.BPoint, enemyValues.APoint) = (enemyValues.APoint, enemyValues.BPoint);
             transform.position = Vector2.MoveTowards(transform.position, enemyValues.BPoint.transform.position, enemyValues.enemySpeed * Time.deltaTime);
@@ -30,4 +50,41 @@ public class EnemyController : MonoBehaviour
         }
     }
 
+    private void PlayerTrackker()
+    {
+        Vector2 direction = enemyValues.IsFacingRight ? Vector2.right : Vector2.left;
+        Vector2 origin = new(transform.position.x, transform.position.y + 0.5f);
+
+        RaycastHit2D hit = Physics2D.Raycast(origin, direction, 10f, enemyValues.playerLayer);
+        Debug.DrawRay(origin, direction * 10f, Color.red);
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.CompareTag("Player"))
+            {
+                transform.position = Vector2.MoveTowards(transform.position, enemyValues.player.transform.position, enemyValues.enemySpeed * Time.deltaTime);
+            }
+        }
+    }
+
+    private void PlayerDetector()
+    {
+        Vector2 direction = enemyValues.IsFacingRight ? Vector2.right : Vector2.left;
+        Vector2 origin = new(transform.position.x, transform.position.y + 0.5f);
+
+        RaycastHit2D hit = Physics2D.Raycast(origin, direction, 10f, enemyValues.playerLayer);
+        Debug.DrawRay(origin, direction * 10f, Color.red);
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.CompareTag("Player"))
+            {
+                enemyValues.IsPlayerInRange = true;
+            }
+            else
+                enemyValues.IsPlayerInRange = false;
+        }
+        else
+            enemyValues.IsPlayerInRange = false;
+    }
 }
--- Assets/Scripts/EnemyScripts/EnemyValues.cs
+++ Assets/Scripts/EnemyScripts/EnemyValues.cs
@@ -5,4 +5,12 @@ public class EnemyValues : MonoBehaviour
     public float enemySpeed = 5f;
     public Transform APoint;
     public Transform BPoint;
+
+    public Rigidbody2D enemyRb;
+    public GameObject player;
+    public LayerMask playerLayer;
+    public SpriteRenderer enemySpriteRenderer;
+
+    public bool IsFacingRight = false;
+    public bool IsPlayerInRange = false;
 }
